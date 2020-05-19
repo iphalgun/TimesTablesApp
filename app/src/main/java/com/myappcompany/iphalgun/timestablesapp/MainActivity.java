76d41a3f6962a9c1c8f4149db502c3903b499d19
iphalgun <phalgun99@gmail.com>
2020-05-19 21:23:21 +0530

    Initial commit

---
+++ app/src/main/java/com/myappcompany/iphalgun/timestablesapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.myappcompany.iphalgun.timestablesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView timesTableListView;
+   // int timesTableNumber;
+
+    public void generateTimesTable(int timesTableNumber)
+    {
+        ArrayList<String> timesTableContent = new ArrayList<>();
+
+        for(int j=1;j<=10;j++)
+        {
+            timesTableContent.add(Integer.toString(j*timesTableNumber));
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,timesTableContent);
+
+        timesTableListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        final SeekBar timesTableSeekBar = findViewById(R.id.timesTablesSeekBar);
+
+        timesTableListView = findViewById(R.id.timesTablesListView);
+
+        int max=20;
+        int startingPosition=10;
+
+        timesTableSeekBar.setMax(max);
+        timesTableSeekBar.setProgress(startingPosition);
+
+        generateTimesTable(startingPosition);
+
+        timesTableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = 1;
+                int timesTableNumber;
+
+                if(i < min){
+                    timesTableNumber = min;
+                    timesTableSeekBar.setProgress(10);
+                }
+                else
+                {
+                    timesTableNumber = i;
+                }
+
+                Log.i("Seekbar Value", Integer.toString(timesTableNumber));
+
+                generateTimesTable(timesTableNumber);
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
